6f102741b873474b8ae3315af82c194004728cdd
ducanh4531 <ducanhle1794@gmail.com>
2023-05-28 15:32:14 +0800

    build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,15 @@ import { HStack, Image } from "@chakra-ui/react";
 
 import ColorModeSwitch from "./ColorModeSwitch";
 import logo from "../assets/logo.webp"; //webp img are highly optimized img for web
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
 	return (
-		<HStack justify="space-between" padding="10px">
+		<HStack padding="10px">
 			{" "}
 			//horizontal stack: all items sorted in horizontal
 			<Image src={logo} boxSize="60px" />
+			<SearchInput />
 			<ColorModeSwitch />
 		</HStack>
 	);
