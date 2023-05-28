f1ba07d1519ec274f9b04658d325396623af26ab
ducanh4531 <ducanhle1794@gmail.com>
2023-05-28 17:55:43 +0800

    clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,6 +5,7 @@ import {
 	Button,
 	HStack,
 	Spinner,
+	Heading,
 } from "@chakra-ui/react";
 
 import useGenres, { Genre } from "../hooks/useGenres";
@@ -23,31 +24,42 @@ const GenreList = ({ onSelectGenre, selectedGenre }: GenreListProps) => {
 	return isLoading ? (
 		<Spinner />
 	) : (
-		<List>
-			{genres.map((genre) => (
-				<ListItem key={genre.id} paddingY="5px">
-					<HStack>
-						<Image
-							boxSize="32px"
-							borderRadius={8}
-							src={getCroppedImageUrl(genre.image_background)}
-						/>
-						<Button
-							fontSize="lg"
-							variant="link"
-							fontWeight={
-								genre.id === selectedGenre?.id
-									? "bold"
-									: "normal"
-							}
-							onClick={() => onSelectGenre(genre)}
-						>
-							{genre.name}
-						</Button>
-					</HStack>
-				</ListItem>
-			))}
-		</List>
+		<>
+			<Heading fontSize="2xl" marginBottom={3}>
+				Genres
+			</Heading>
+			<List>
+				{genres.map((genre) => (
+					<ListItem key={genre.id} paddingY="5px">
+						<HStack>
+							<Image
+								boxSize="32px"
+								borderRadius={8}
+								// preserve img's aspect ratio fit in container and img not be broken
+								objectFit="cover"
+								src={getCroppedImageUrl(genre.image_background)}
+							/>
+							{/* by default, buttons don't wrap text */}
+							<Button
+								fontSize="lg"
+								// normal as opposed to nowrap
+								whiteSpace="normal"
+								textAlign="left"
+								variant="link"
+								fontWeight={
+									genre.id === selectedGenre?.id
+										? "bold"
+										: "normal"
+								}
+								onClick={() => onSelectGenre(genre)}
+							>
+								{genre.name}
+							</Button>
+						</HStack>
+					</ListItem>
+				))}
+			</List>
+		</>
 	);
 };
 
